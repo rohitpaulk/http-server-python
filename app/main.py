27eaf6ad353797cccf9c702a69914649de04894e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-11 19:10:52 +0100

    Fix server_socket.accept() to properly handle client connection

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()  # wait for client
+    print("Client connected", addr)
+
+    conn.recv(1024)
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
